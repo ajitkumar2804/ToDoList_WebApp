3b68d570fbbd3bb379e815078b387670ff99f208
ajitkumar2804 <ajitjagtap67@gmail.com>
2021-10-22 15:42:00 +0530

    Done with add button transition

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const btninput = document.querySelector(".input input");
 const addbtn = document.querySelector(".input button");
 
+
+//funtion for add button transition 
 btninput.onkeyup = function(){
     let user = btninput.value;
     if(user.trim() !=0){
         addbtn.classList.add("active");
+    }else{
+        addbtn.classList.remove("active");
     }
 }
  
