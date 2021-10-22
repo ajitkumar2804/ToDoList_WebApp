e2d3c503aec4b2b2162dd21aefcf7d6ea8a2407e
ajitkumar2804 <ajitjagtap67@gmail.com>
2021-10-22 16:36:17 +0530

    Done with Add Button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btninput = document.querySelector(".input input");
 const addbtn = document.querySelector(".input button");
-
+const list = document.querySelector(".list");
+const btnClear = document.querySelector("#clear");
 
 //funtion for add button transition 
 btninput.onkeyup = function(){
@@ -11,4 +12,35 @@ btninput.onkeyup = function(){
         addbtn.classList.remove("active");
     }
 }
+showTask();
  
+// add funtion 
+addbtn.onclick = function(){
+    let user = btninput.value;
+    let getLocalStorage = localStorage.getItem("New Task");
+    if(getLocalStorage== null){
+        listArr =[];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(user);
+    localStorage.setItem("New Task",JSON.stringify(listArr));
+    showTask();
+} 
+
+//Funtion for SHowing the task
+function showTask(){
+    let getLocalStorage = localStorage.getItem("New Task");
+    if(getLocalStorage== null){
+        listArr =[];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    let newtag = '';
+    listArr.forEach((element,index) => {
+        newtag += `<li>${element}<span><i class="fas fa-trash"></i></span></li>`
+    });
+    list.innerHTML = newtag;
+    btninput.value = "";
+}
+
